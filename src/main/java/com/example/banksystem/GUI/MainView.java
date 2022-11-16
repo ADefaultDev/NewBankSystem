e48e8776034459ac0df6dedde04dc8154c870bb4
ADefaultDev <im7ckngmad@gmail.com>
2022-11-16 23:57:01 +0300

    Main page prototype created, sql output deleted

--- src/main/java/com/example/banksystem/GUI/MainView.java
+++ src/main/java/com/example/banksystem/GUI/MainView.java
@@ -1,14 +1,31 @@
 package com.example.banksystem.GUI;
 
-
+import com.example.banksystem.credit.Credit;
+import com.example.banksystem.credit.CreditRepository;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Route
 public class MainView extends VerticalLayout {
-    public MainView(){
-            add(new Button("Hello world", event -> Notification.show("Hello world!",1, Notification.Position.MIDDLE)));
+
+    private final CreditRepository creditRepository;
+    final Grid<Credit> grid;
+    public MainView(CreditRepository creditRepository){
+        this.creditRepository = creditRepository;
+        this.grid = new Grid<>(Credit.class);
+        add(grid);
+        add(new Button("Show credits", event -> showCredits()));
     }
+
+    public void showCredits(){
+        grid.setItems(creditRepository.findAll());
+    }
+
+
 }
--- src/main/java/com/example/banksystem/credit/CreditRepository.java
+++ src/main/java/com/example/banksystem/credit/CreditRepository.java
@@ -3,8 +3,9 @@ package com.example.banksystem.credit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CreditRepository extends JpaRepository<Credit, Long> {
-
 
 }
